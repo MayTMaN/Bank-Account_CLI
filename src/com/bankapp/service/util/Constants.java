7f205536001184d8479bf8bbc000c1ce5174ac12
MayTMaN <dima.demchuk.277@gmail.com>
2026-06-13 21:42:57 +0300

    remove old non-Maven source directory

--- src/com/bankapp/service/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.bankapp.service.util;
-
-public class Constants {
-
-    public static final String DB_FILE = "database.txt";
-    public static final double STARTING_BALANCE = 0.0;
-
-}
